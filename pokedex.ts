f3d47c00e6b53c8d48a591fa5309f28ee76b79c0
Rezzo64 <rezzo64@gmail.com>
2023-12-05 12:12:26 +0000

    JEP: add real Pokemon

--- pokedex.ts
+++ pokedex.ts
@@ -1,3 +1,396 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
-
+	// REAL
+	pichu: {
+		inherit: true,
+		otherFormes: ["Pichu-Spiky-eared"],
+		formeOrder: ["Pichu", "Pichu-Spiky-eared"],
+	},
+	raichualola: {
+		inherit: true,
+		gen: 2
+	},
+	leafeon: {
+		inherit: true,
+		gen: 2
+	},
+	glaceon: {
+		inherit: true,
+		gen: 2
+	},
+	sylveon: {
+		inherit: true,
+		gen: 2
+	},
+	rattataalola: {
+		inherit: true,
+		gen: 2
+	},
+	raticatealola: {
+		inherit: true,
+		gen: 2
+	},
+	meowthalola: {
+		inherit: true,
+		gen: 2
+	},
+	meowthgalar: {
+		inherit: true,
+		gen: 2
+	},
+	persianalola: {
+		inherit: true,
+		gen: 2
+	},
+	perrserker: {
+		inherit: true,
+		gen: 2
+	},
+	geodudealola: {
+		inherit: true,
+		gen: 2
+	},
+	graveleralola: {
+		inherit: true,
+		gen: 2
+	},
+	golemalola: {
+		inherit: true,
+		gen: 2
+	},
+	sandshrewalola: {
+		inherit: true,
+		gen: 2
+	},
+	sandslashalola: {
+		inherit: true,
+		gen: 2
+	},
+	vulpixalola: {
+		inherit: true,
+		gen: 2
+	},
+	ninetalesalola: {
+		inherit: true,
+		gen: 2
+	},
+	annihilape: {
+		inherit: true,
+		gen: 2
+	},
+	lickilicky: {
+		inherit: true,
+		gen: 2
+	},
+	growlithehisui: {
+		inherit: true,
+		gen: 2
+	},
+	arcaninehisui: {
+		inherit: true,
+		gen: 2
+	},
+	farfetchdgalar: {
+		inherit: true,
+		gen: 2
+	},
+	sirfetchd: {
+		inherit: true,
+		gen: 2
+	},
+	diglettalola: {
+		inherit: true,
+		gen: 2
+	},
+	dugtrioalola: {
+		inherit: true,
+		gen: 2
+	},
+	wiglett: {
+		inherit: true,
+		gen: 2
+	},
+	wugtrio: {
+		inherit: true,
+		gen: 2
+	},
+	voltorbhisui: {
+		inherit: true,
+		gen: 2
+	},
+	electrodehisui: {
+		inherit: true,
+		gen: 2
+	},
+	toedscool: {
+		inherit: true,
+		gen: 2
+	},
+	toedscruel: {
+		inherit: true,
+		gen: 2
+	},
+	magnezone: {
+		inherit: true,
+		gen: 2
+	},
+	slowpokegalar: {
+		inherit: true,
+		gen: 2
+	},
+	slowbrogalar: {
+		inherit: true,
+		gen: 2
+	},
+	slowkinggalar: {
+		inherit: true,
+		gen: 2
+	},
+	ponytagalar: {
+		inherit: true,
+		gen: 2
+	},
+	rapidashgalar: {
+		inherit: true,
+		gen: 2
+	},
+	marowakalola: {
+		inherit: true,
+		gen: 2
+	},
+	grimeralola: {
+		inherit: true,
+		gen: 2
+	},
+	mukalola: {
+		inherit: true,
+		gen: 2
+	},
+	weezinggalar: {
+		inherit: true,
+		gen: 2
+	},
+	tangrowth: {
+		inherit: true,
+		gen: 2
+	},
+	exeggutoralola: {
+		inherit: true,
+		gen: 2
+	},
+	rhyperior: {
+		inherit: true,
+		gen: 2
+	},
+	kleavor: {
+		inherit: true,
+		gen: 2
+	},
+	taurospaldeacombat: {
+		inherit: true,
+		gen: 2
+	},
+	taurospaldeablaze: {
+		inherit: true,
+		gen: 2
+	},
+	taurospaldeaaqua: {
+		inherit: true,
+		gen: 2
+	},
+	happiny: {
+		inherit: true,
+		gen: 2
+	},
+	mimejr: {
+		inherit: true,
+		gen: 2
+	},
+	mrmimegalar: {
+		inherit: true,
+		gen: 2
+	},
+	mrrime: {
+		inherit: true,
+		gen: 2
+	},
+	electivire: {
+		inherit: true,
+		gen: 2
+	},
+	magmortar: {
+		inherit: true,
+		gen: 2
+	},
+	porygonz: {
+		inherit: true,
+		gen: 2
+	},
+	munchlax: {
+		inherit: true,
+		gen: 2
+	},
+	screamtail: {
+		inherit: true,
+		gen: 2
+	},
+	sandyshocks: {
+		inherit: true,
+		gen: 2
+	},
+	articunogalar: {
+		inherit: true,
+		gen: 2
+	},
+	zapdosgalar: {
+		inherit: true,
+		gen: 2
+	},
+	moltresgalar: {
+		inherit: true,
+		gen: 2
+	},
+	meltan: {
+		inherit: true,
+		gen: 2
+	},
+	melmetal: {
+		inherit: true,
+		gen: 2
+	},
+	typhlosionhisui: {
+		inherit: true,
+		gen: 2
+	},
+	ursaluna: {
+		inherit: true,
+		gen: 2
+	},
+	ursalunabloodmoon: {
+		inherit: true,
+		gen: 2
+	},
+	wynaut: {
+		inherit: true,
+		gen: 2
+	},
+	dudunsparce: {
+		inherit: true,
+		gen: 2
+	},
+	dudunsparcethreesegment: {
+		inherit: true,
+		gen: 2
+	},
+	farigiraf: {
+		inherit: true,
+		gen: 2
+	},
+	azurill: {
+		inherit: true,
+		gen: 2
+	},
+	wooperpaldea: {
+		inherit: true,
+		gen: 2
+	},
+	clodsire: {
+		inherit: true,
+		gen: 2
+	},
+	yanmega: {
+		inherit: true,
+		gen: 2
+	},
+	ambipom: {
+		inherit: true,
+		gen: 2
+	},
+	bonsly: {
+		inherit: true,
+		gen: 2
+	},
+	mantyke: {
+		inherit: true,
+		gen: 2
+	},
+	corsolagalar: {
+		inherit: true,
+		gen: 2
+	},
+	cursola: {
+		inherit: true,
+		gen: 2
+	},
+	wyrdeer: {
+		inherit: true,
+		gen: 2
+	},
+	honchkrow: {
+		inherit: true,
+		gen: 2
+	},
+	mismagius: {
+		inherit: true,
+		gen: 2
+	},
+	qwilfishhisui: {
+		inherit: true,
+		gen: 2
+	},
+	overqwil: {
+		inherit: true,
+		gen: 2
+	},
+	mamoswine: {
+		inherit: true,
+		gen: 2
+	},
+	sneaselhisui: {
+		inherit: true,
+		gen: 2
+	},
+	weavile: {
+		inherit: true,
+		gen: 2
+	},
+	sneasler: {
+		inherit: true,
+		gen: 2
+	},
+	gliscor: {
+		inherit: true,
+		gen: 2
+	},
+	togekiss: {
+		inherit: true,
+		gen: 2
+	},
+	greattusk: {
+		inherit: true,
+		gen: 2
+	},
+	fluttermane: {
+		inherit: true,
+		gen: 2
+	},
+	irontreads: {
+		inherit: true,
+		gen: 2
+	},
+	ironbundle: {
+		inherit: true,
+		gen: 2
+	},
+	ironthorns: {
+		inherit: true,
+		gen: 2
+	},
+	walkingwake: {
+		inherit: true,
+		gen: 2
+	},
+	ragingbolt: {
+		inherit: true,
+		gen: 2
+	},
 };
